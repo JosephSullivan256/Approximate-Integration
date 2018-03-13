08d94a4f257d658e5173c7946a9b77a5eb12917f
Joseph Sullivan <youknowjoejoe@gmail.com>
2018-03-12 22:09:22 -0700

    addded all defined rules to main

--- src/com/josephsullivan256/gmail/approxint/Main.java
+++ src/com/josephsullivan256/gmail/approxint/Main.java
@@ -3,24 +3,30 @@ package com.josephsullivan256.gmail.approxint;
 public class Main {
 
 	public static void main(String[] args) {
-		Function f = (x)->Math.exp(x)/(1+x*x);
+		Function f = (x)->x*x*x*x;
 		double a = 0;
 		double b = 2;
 		int n = 10;
 		
 		ApproximateIntegral integral = new ApproximateIntegral(a,b,n,f);
 		
+		double r = integral.evaluate(IntegrationMethod.r);
+		System.out.println("Right Endpoints: " + r);
+		double l = integral.evaluate(IntegrationMethod.l);
+		System.out.println("Left  Endpoints: " + l);
 		double t = integral.evaluate(IntegrationMethod.t);
-		System.out.println("Trapezoid Rule: " + t);
+		System.out.println("Trapezoid  Rule: " + t);
 		double m = integral.evaluate(IntegrationMethod.m);
-		System.out.println("Midpoint  Rule: " + m);
+		System.out.println("Midpoint   Rule: " + m);
 		double s = integral.evaluate(IntegrationMethod.s);
-		System.out.println("Simpson's Rule: " + s);
+		System.out.println("Simpson's  Rule: " + s);
 		
 		System.out.println();
 		
-		double value = 0;
+		double value = 32.0/5.0;
 		
+		System.out.println("Er :" + (value-r));
+		System.out.println("El :" + (value-l));
 		System.out.println("Et :" + (value-t));
 		System.out.println("Em :" + (value-m));
 		System.out.println("Es :" + (value-s));
